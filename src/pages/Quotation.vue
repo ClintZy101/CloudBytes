83b22322cedc6002c83b88fd61d120eee4ef09de
Ivan Arnie Ranario <ivanarnieranario@outlook.com>
2021-05-03 14:52:49 +0800

    added cordova and update total on quotation

--- src/pages/Quotation.vue
+++ src/pages/Quotation.vue
@@ -20,6 +20,7 @@
           <q-item-section>
             <q-item-label lines="1">{{ item.details.itemName }}</q-item-label>
             <q-item-label caption>{{ item.details.category }}</q-item-label>
+            <q-item-label caption>Cost: {{ getFormattedCost(item) }} | Interest: {{ getFormattedInterest(item) }}</q-item-label>
           </q-item-section>
 
           <q-item-section side top>
@@ -34,9 +35,9 @@
      <q-card class="my-card q-mt-md q-pa-xs" v-if="items.length > 0">
       <q-card-section>
         <div class="text-subtitle2 text-info">Note: Slide right the item to remove or decrease it.</div>
-        <div class="text-subtitle1">Total Cost: <q-chip color="secondary" text-color="white">{ on working }</q-chip></div>
-        <div class="text-subtitle1">Total Price: <q-chip color="secondary" text-color="white">{ on working }</q-chip></div>
-        <div class="text-subtitle1">Total Interest: <q-chip color="secondary" text-color="white">{ on working }</q-chip></div>
+        <div class="text-subtitle1">Total Cost: <q-chip color="secondary" text-color="white">{{ getFormattedTotalCost }}</q-chip></div>
+        <div class="text-subtitle1">Total Price: <q-chip color="secondary" text-color="white">{{ getFormattedTotalPrice }}</q-chip></div>
+        <div class="text-subtitle1">Total Interest: <q-chip color="secondary" text-color="white">{{ getTotalInterest }}</q-chip></div>
       </q-card-section>
 
       <q-separator />
@@ -62,39 +63,111 @@ export default {
       itemListDialog: false,
       priceType: 'newPrice',
       items: [],
+      totalNewPrice: 0,
+      totalMainPrice: 0,
+      totalBranchPrice: 0,
+      totalMainCost: 0,
+      totalBranchCost: 0,
+      totalInterest: 0
     }
+  },
+   beforeDestroy () {
+    clearTimeout(this.timer)
   },
   computed: {
     getItems () {
       return this.items;
+    },
+    getFormattedTotalPrice () {
+      return PESO(this.getTotalPrice).format();
+    },
+    getFormattedTotalCost () {
+      return PESO(this.getTotalCost).format();
+    },
+    getTotalPrice () {
+      switch(this.priceType) {
+        case 'newPrice':
+          return this.totalNewPrice;
+        case 'mainPrice':
+          return this.totalMainPrice;
+        case 'branchPrice':
+          return this.totalBranchPrice;
+        default:
+          return 0;
+      }
+    },
+    getTotalCost () {
+      switch(this.priceType) {
+        case 'newPrice':
+        case 'mainPrice':
+          return this.totalMainCost;
+        case 'branchPrice':
+          return this.totalBranchCost;
+        default:
+          return 0;
+      }
+    },
+    getTotalInterest () {
+      return PESO(this.getTotalPrice - this.getTotalCost).format();
     }
   },
   methods: {
+    finalize (reset) {
+      this.timer = setTimeout(() => {
+        reset()
+      }, 300)
+    },
+    minusTotal(item) {
+      this.totalNewPrice -= item.newPricing.price;
+      this.totalMainPrice -= item.oldPricing.main.price;
+      this.totalBranchPrice -= item.oldPricing.branch.price;
+
+      this.totalMainCost -= item.oldPricing.main.cost;
+      this.totalBranchCost -= item.oldPricing.branch.cost;
+    },
+    addTotal(item) {
+      this.totalNewPrice += item.newPricing.price;
+      this.totalMainPrice += item.oldPricing.main.price;
+      this.totalBranchPrice += item.oldPricing.branch.price;
+
+      this.totalMainCost += item.oldPricing.main.cost;
+      this.totalBranchCost += item.oldPricing.branch.cost;
+    },
     removeItem ({ reset }, id) {
       if (this.items.length > 0) {
         const find = _.find(this.items, x => x.id === id);
         if (find !== undefined && find.quantity === 1) {
           const findIndex = _.findIndex(this.items, x => x.id === id);
+          this.minusTotal(this.items[findIndex]);
           this.items.splice(findIndex, 1);
         } else {
           find.quantity--;
-          reset();
+          this.minusTotal(find);
           this.$forceUpdate(); 
+          this.finalize(reset);
         }
       }
     },
     clear () {
       this.items = [];
+      this.totalNewPrice = 0;
+      this.totalMainPrice = 0;
+      this.totalBranchPrice = 0;
+      this.totalMainCost = 0;
+      this.totalBranchCost = 0;
+      this.totalInterest = 0;
     },
     itemSelect(data) {
       if (this.items.length > 0) {
         const findIndex = _.findIndex(this.items, x => x.id === data.id);
         if (findIndex > -1) {
           this.items[findIndex].quantity++;
+          this.addTotal(this.items[findIndex]);
           return;
         }
       } 
       data.quantity = 1;
+      this.addTotal(data);
       this.items.push(data);
     },
     getPrice(item) {
@@ -106,6 +179,23 @@ export default {
         case 'branchPrice':
           return item.oldPricing.branch.price;
       }
+    },
+    getCost(item) {
+      switch(this.priceType) {
+        case 'newPrice':
+        case 'mainPrice':
+          return item.oldPricing.main.cost;
+        case 'branchPrice':
+          return item.oldPricing.branch.cost;
+      }
+    },
+    getFormattedCost(item) {
+      return PESO(this.getCost(item) * item.quantity).format();
+    },
+    getFormattedInterest(item) {
+      const cost = this.getCost(item) * item.quantity;
+      const price = this.getPrice(item) * item.quantity;
+      return PESO(price - cost).format();
     },
     getFormattedPrice(item) {
       return PESO(this.getPrice(item)).format();
